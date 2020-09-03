c4cf3534de5385980ed45954520bb3d167059282
Camilo6 <trresqwe@gmail.com>
2020-09-03 17:35:48 -0500

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length == 1:
+        print("{} {}.".format(length - 1, "arguments"))
+    elif length == 2:
+        print("{} {}:".format(length - 1, "argument"))
+        print("{}: {}".format(length - 1, argv[1]))
+    else:
+        for i in range(length):
+            if i == 0:
+                print("{} {}:".format(length - 1, "arguments"))
+            else:
+                print("{}: {}".format(i, argv[i]))
