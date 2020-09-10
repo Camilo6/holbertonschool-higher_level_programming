81816dbc676f6325c17eb5f429dd69921f94102e
Camilo6 <trresqwe@gmail.com>
2020-09-10 18:23:34 -0500

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if my_dict is None:
+        return None
+    max_score = 0
+    key = None
+    for k, v in my_dict.items():
+        if v > max_score:
+            max_score = v
+            key = k
+    return key
