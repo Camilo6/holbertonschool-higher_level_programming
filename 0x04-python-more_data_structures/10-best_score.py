cf66d7b7b45e07cdff3ee09757e4883f6405f781
Camilo6 <trresqwe@gmail.com>
2020-09-10 18:42:29 -0500

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if my_dict is None:
+    if a_dictionary is None:
         return None
     max_score = 0
     key = None
-    for k, v in my_dict.items():
+    for k, v in a_dictionary.items():
         if v > max_score:
             max_score = v
             key = k
