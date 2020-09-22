a7d0c0d9cbdc33d03b730279d3830d34d8c7aa82
Camilo6 <trresqwe@gmail.com>
2020-09-22 17:00:07 -0500

    update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+This module is from task 5
+"""
+
+
+class Square:
+    """
+    This class defines a square based on 4-square
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for e in range(0, self.__size):
+                print("".join("{}".format("#" * self.__size)))
