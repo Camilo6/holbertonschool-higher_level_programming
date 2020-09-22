1bd0b7aa6e6df8182b490002c431d79c762d556b
Camilo6 <trresqwe@gmail.com>
2020-09-22 13:43:04 -0500

    0 y 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module "0-square" is task 0
+"""This module  is from task 0
 """
 class Square:
 	"""This class defines a square"""
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""This module is from task 1"""
 class Square:
-	"""This module is from task 1"""
+	"""This class define a size square"""
 	def __init__(self, _Square__size):
 		self._Square__size = _Square__size
