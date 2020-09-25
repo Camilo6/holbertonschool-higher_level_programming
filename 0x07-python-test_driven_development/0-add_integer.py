576c71b5ff3b9bcc22b96b0b71e143ad55f2687c
Camilo6 <trresqwe@gmail.com>
2020-09-25 12:50:19 -0500

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module is from task 0"""
+
+
+def add_integer(a, b=98):
+    """This function adds two integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a_int = int(a)
+    b_int = int(b)
+    return(a_int + b_int)
