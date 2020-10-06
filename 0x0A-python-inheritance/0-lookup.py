17b69c5da700deb37f14bec63921ec04999853a7
Camilo6 <trresqwe@gmail.com>
2020-10-06 17:11:26 -0500

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module is from task 0"""
+
+
+def lookup(obj):
+    """This function returns the list of
+    available attributes and methods of an object"""
+    return dir(obj)
