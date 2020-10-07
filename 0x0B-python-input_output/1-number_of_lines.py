a52016b17aa8cca4ad8b8f4226fb884379176c6f
Camilo6 <trresqwe@gmail.com>
2020-10-07 17:45:28 -0500

    1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""This module is from task 1"""
+
+
 def number_of_lines(filename=""):
+    """This function prints number of line of a file"""
+    with open(filename, 'r', encoding='utf-8') as fi:
+        count = 0
+        for line in fi:
+            count += 1
+    return(count)
