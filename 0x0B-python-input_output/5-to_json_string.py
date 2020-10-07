aca7a41ab09f91359158b4a7807e1e907f5e3e56
Camilo6 <trresqwe@gmail.com>
2020-10-07 18:44:21 -0500

    5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+import json
+"""This module is from task 5"""
+
+
 def to_json_string(my_obj):
+    """This function that returns
+    the JSON representation of an object (string)
+    """
+    return(json.dumps(my_obj))
