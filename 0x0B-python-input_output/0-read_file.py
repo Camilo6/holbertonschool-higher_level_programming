b87efdd518b38ca2bf97a3b00c8bdd9fa9842952
Camilo6 <trresqwe@gmail.com>
2020-10-07 17:36:55 -0500

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r') as filee:
-        for line in filee:
-            line = line.strip()
-            print(line)
+    with open(filename, 'r') as fi:
+        for line in fi:
+            print(line, end="")
