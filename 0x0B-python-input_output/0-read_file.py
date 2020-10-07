5d7651c68166f16af4fdfd7ad9d02ad4c77efa9e
Camilo6 <trresqwe@gmail.com>
2020-10-07 17:41:54 -0500

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This module is from task 0"""
+
+
 def read_file(filename=""):
+    """This function prints content of a file"""
     with open(filename, 'r', encoding='utf-8') as fi:
             print(fi.read(), end="")
