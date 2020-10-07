7084283fb155077cec52dbe0ff5cc90ac900b3c5
Camilo6 <trresqwe@gmail.com>
2020-10-07 18:48:31 -0500

    5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 import json
-"""This module is from task 5"""
+"""
+This is module 5-to_json_string
+This module contains one function `to_json_string`
+"""
 
 
 def to_json_string(my_obj):
-    """This function that returns
-    the JSON representation of an object (string)
+    """
+    function that returns JSON representation
+    of an object (string):
+    You don't need to manage exceptions if
+    the object can't be serialized.
+    Args:
+    my_obj
+    Return:
+    JSON representation of object
     """
     return json.dumps(my_obj)
