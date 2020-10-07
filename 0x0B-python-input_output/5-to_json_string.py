23c5e493b1c3e266b8120db2a25e8a685e4f3e88
Camilo6 <trresqwe@gmail.com>
2020-10-07 18:46:49 -0500

    5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -7,4 +7,4 @@ def to_json_string(my_obj):
     """This function that returns
     the JSON representation of an object (string)
     """
-    return(json.dumps(my_obj))
+    return json.dumps(my_obj)
