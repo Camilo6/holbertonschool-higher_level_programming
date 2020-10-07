0987a920bfb9ec929f7f978de9fe55c908724d50
Camilo6 <trresqwe@gmail.com>
2020-10-07 18:17:05 -0500

    4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,6 +6,6 @@ def append_write(filename="", text=""):
     """function that writes a string to
     a text file (UTF8) and returns the
     number of characters written"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         char_count = f.write(text)
         return (char_count)
