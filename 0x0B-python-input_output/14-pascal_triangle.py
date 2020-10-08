6e4ec4c9102273f1a70d86bc17946abadd830e30
Camilo6 <trresqwe@gmail.com>
2020-10-08 18:35:51 -0500

    14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,15 +8,14 @@ def pascal_triangle(n):
     """This function returns a list of lists
     of integers representing the Pascal’s triangle of n:
     """
+
     if n <= 0:
-	    return lista
-
-    lista = [[1], [1, 1]]
-    for i in range(1, n):
-        linea = [1]
-        for j in range(0, len(lista[i])-1):
-            linea.extend([lista[i][j] + lista[i][j+1]])
-        linea += [1]
-        lista.append(linea)
-
-    return lista
+        lista = []
+        return lista
+    else:
+        lista = []
+        for i in range(n):
+            lista.append([1] * (i+1))
+            for j in range(1, len(lista[i])-1):
+                lista[i][j] = lista[i - 1][j] + lista[i - 1][j - 1]
+        return lista
