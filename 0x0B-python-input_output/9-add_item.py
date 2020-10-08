d57524125c7ac651966e7fff3f283ac672256a30
Camilo6 <trresqwe@gmail.com>
2020-10-07 20:35:21 -0500

    9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""
+This module is from task 9
+"""
+
+import sys
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+
+def load_add_save(filename, args):
+    try:
+        new_json_object = load_from_json_file(filename)
+    except FileNotFoundError:
+        new_json_object = []
+    new_json_object = save_to_json_file(new_json_object + args, filename)
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    load_add_save(filename, sys.argv[1:])
