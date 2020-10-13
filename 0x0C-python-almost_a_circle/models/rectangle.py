db730f9ad1e364f9d2c46bf7c072aeb93cc244b2
camilo <trresqwe@gmail.com>
2020-10-13 18:50:00 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,50 +63,5 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """return area"""
         return (self.__width * self.__height)
-
-    def display(self):
-        """prints in stdout the Rectangle instance with the character #"""
-        for i in range(self.__y):
-            print()
-        for i in range(self.__height):
-            print(self.__x * " " + "#" * self.__width)
-
-    def __str__(self):
-        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
-                                                self.id, self.__x, self.__y,
-                                                self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        if args:
-            for arg in range(len(args)):
-                if arg == 0:
-                    self.id = args[arg]
-                if arg == 1:
-                    self.__width = args[arg]
-                if arg == 2:
-                    self.__height = args[arg]
-                if arg == 3:
-                    self.__x = args[arg]
-                if arg == 4:
-                    self.__y = args[arg]
-        else:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'width' in kwargs:
-                self.width = kwargs['width']
-            if 'height' in kwargs:
-                self.height = kwargs['height']
-            if 'x' in kwargs:
-                self.x = kwargs['x']
-            if 'y' in kwargs:
-                self.y = kwargs['y']
-
-    def to_dictionary(self):
-        new_dict = {}
-        new_dict['id'] = self.id
-        new_dict['width'] = self.width
-        new_dict['height'] = self.height
-        new_dict['x'] = self.x
-        new_dict['y'] = self.y
-        return new_dict
