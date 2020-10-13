b4310026a485ab1609eee7b7677dccfa20dfed79
camilo <trresqwe@gmail.com>
2020-10-13 16:05:05 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""class rectangle"""
-
-
+"""subclass rectangle"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """this class inherits from Base"""
-
+    """this subclass inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
