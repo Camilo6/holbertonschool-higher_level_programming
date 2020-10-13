b74bef7777b01aa9c0a18bfc1e2289a771832967
camilo <trresqwe@gmail.com>
2020-10-12 21:29:44 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,10 +73,14 @@ class Rectangle(Base):
         return (self.__width * self.__height)
 
     def display(self):
-        if self.__y > 0:
-            print("\n" * (self.__y), end="")
+        print('\n' * self.__y, end="")
         for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+            print(' ' * self.__x, end='')
+            for j in range(self.__width):
+                if j != self.__width - 1:
+                    print('#', end="")
+                else:
+                    print("#")
 
     def __str__(self):
         return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
