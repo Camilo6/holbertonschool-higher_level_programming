65161ab8c7a560d585bffd0b89daa366d99cd864
camilo <trresqwe@gmail.com>
2020-10-13 18:37:28 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ from models.base import Base
 class Rectangle(Base):
     """class rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-	"""Constructor"""
+        """Constructor"""
         self.width = width
         self.height = height
         self.x = x
