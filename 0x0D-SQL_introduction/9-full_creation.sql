54840e1e853344a93e42288c7189f426523b4511
Camilo <trresqwe@gmail.com>
2020-11-16 19:08:44 -0500

    Update 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- that creates a table called second_table
+-- Creates a table called second_table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (1, 'John', 10);
 INSERT INTO second_table VALUES (2, 'Alex', 3);
